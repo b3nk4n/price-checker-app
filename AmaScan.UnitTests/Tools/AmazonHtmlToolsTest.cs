925253a7e22d9efa4508bb2432a4de9b73f12959
bsautermeister <apps@bsautermeister.de>
2017-01-26 15:47:53 +0100

    Added new unit tests, since a scanned code is not scanning the detail page, but a list of products.

--- AmaScan.UnitTests/Tools/AmazonHtmlToolsTest.cs
+++ AmaScan.UnitTests/Tools/AmazonHtmlToolsTest.cs
@@ -18,7 +18,7 @@ namespace AmaScan.UnitTests.Tools
         {
             var file = await StorageService.GetFileFromApplicationAsync("/Assets/Html/camera.html");
             var htmlContent = await StorageService.ReadFile(file);
-            var title = AmazonHtmlTools.ExtractTextFromHtml(htmlContent, "productTitle");
+            var title = AmazonHtmlTools.ExtractTextFromHtmlInDetailPage(htmlContent, "productTitle");
 
             Assert.AreEqual("Sony Alpha 6000 Systemkamera (24 Megapixel, 7,6 cm (3\") LCD-Display, Exmor APS-C Sensor, Full-HD, High Speed Hybrid AF) inkl. SEL-P1650 Objektiv schwarz", title);
         }
@@ -28,7 +28,7 @@ namespace AmaScan.UnitTests.Tools
         {
             var file = await StorageService.GetFileFromApplicationAsync("/Assets/Html/camera.html");
             var htmlContent = await StorageService.ReadFile(file);
-            var uri = AmazonHtmlTools.ExtractImageUriFromHtml(htmlContent);
+            var uri = AmazonHtmlTools.ExtractImageUriFromHtmlInDetailPage(htmlContent);
 
             Assert.AreEqual("https://images-na.ssl-images-amazon.com/images/I/518a97hQRvL._SX355_.jpg", uri.AbsoluteUri);
         }
@@ -42,7 +42,7 @@ namespace AmaScan.UnitTests.Tools
         {
             var file = await StorageService.GetFileFromApplicationAsync("/Assets/Html/accu.html");
             var htmlContent = await StorageService.ReadFile(file);
-            var title = AmazonHtmlTools.ExtractTextFromHtml(htmlContent, "productTitle");
+            var title = AmazonHtmlTools.ExtractTextFromHtmlInDetailPage(htmlContent, "productTitle");
 
             Assert.AreEqual("Lenovo Think Pad Battery (9 Cell)", title);
         }
@@ -52,9 +52,33 @@ namespace AmaScan.UnitTests.Tools
         {
             var file = await StorageService.GetFileFromApplicationAsync("/Assets/Html/accu.html");
             var htmlContent = await StorageService.ReadFile(file);
-            var uri = AmazonHtmlTools.ExtractImageUriFromHtml(htmlContent);
+            var uri = AmazonHtmlTools.ExtractImageUriFromHtmlInDetailPage(htmlContent);
 
             Assert.AreEqual("https://images-na.ssl-images-amazon.com/images/I/31s%2BtYHUYpL._SX425_.jpg", uri.AbsoluteUri);
+        }
+
+        #endregion
+
+        #region ZeitWissen
+
+        [TestMethod]
+        public async Task TestZeitWissenExtractProductTitle()
+        {
+            var file = await StorageService.GetFileFromApplicationAsync("/Assets/Html/zeitwissen.html");
+            var htmlContent = await StorageService.ReadFile(file);
+            var title = AmazonHtmlTools.ExtractTextFromHtml(htmlContent, "productTitle");
+
+            Assert.AreEqual("Die Zeit Wissen - Woolen wir zu Perfekt sein", title);
+        }
+
+        [TestMethod]
+        public async Task TestZeitWissenExtractImageUri()
+        {
+            var file = await StorageService.GetFileFromApplicationAsync("/Assets/Html/zeitwissen.html");
+            var htmlContent = await StorageService.ReadFile(file);
+            var uri = AmazonHtmlTools.ExtractImageUriFromHtml(htmlContent);
+
+            Assert.AreEqual("https://images-eu.ssl-images-amazon.com/images/I/61mLbN-4bkL._AC_US174_.jpg", uri.AbsoluteUri);
         }
 
         #endregion
